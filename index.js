f1efa1837720f0aa411bb292a3d43c09ec5cf738
dan <danielkarongoi@gmail.com>
2025-03-29 06:54:16 +0300

    added chnages

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const tutorials = [
   "what does the this keyword mean?",
-  "What is the Constructor OO pattern?",
-  "implementing Blockchain Web API",
-  "The Test Driven Development Workflow",
-  "What is NaN and how Can we Check for it",
-  "What is the difference between stopPropagation and preventDefault?",
-  "Immutable State and Pure Functions",
+  "what is the constructor oo pattern?",
+  "implementing blockchain web api",
+  "the test driven development workflow",
+  "what is nan and how can we check for it",
+  "What Is The Difference Between StopPropagation And PreventDefault?",
+  "immutable state and pure functions",
   "what is the difference between == and ===?",
   "what is the difference between event capturing and bubbling?",
-  "what is JSONP?",
+  "what is JSONP?"
 ];
 
-function titleCased() {
-  return tutorials.map((tutorial) => {
-    return tutorial
-      .split(" ")
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(" ");
+const titleCased = () => {
+  return tutorials.map(tutorial => {
+    return tutorial.split(" ").map(word => {
+      let lowerCaseWord = word.toLowerCase();
+      if (lowerCaseWord === "oo") return "OO";
+      if (lowerCaseWord === "api") return "API";
+      if (lowerCaseWord === "nan") return "NaN";
+      if (lowerCaseWord === "stoppropagation") return "StopPropagation";
+      if (lowerCaseWord === "preventdefault") return "PreventDefault"; // Fix here
+      if (lowerCaseWord === "jsonp") return "JSONP"; // Fix here
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
   });
-}
+};
 
 console.log(titleCased());
